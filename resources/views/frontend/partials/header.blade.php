c6202ac8b5483d572f5bccc8d7654980a5728d3d
emon21 <emonbd66@gmail.com>
2025-11-27 04:14:30 +0600

    chore:Frontend Home Page Difarent Section and hedaer menu Customize

--- resources/views/frontend/partials/header.blade.php
+++ resources/views/frontend/partials/header.blade.php
@@ -1,32 +1,66 @@
-<!DOCTYPE html>
-<html lang="en">
+<header class="site-header lonyo-header-section light-bg" id="sticky-menu">
+  <div class="container">
+    <div class="row gx-3 align-items-center justify-content-between">
+      <div class="col-8 col-sm-auto ">
+        <div class="header-logo1 ">
+          <a href="{{ url('/') }}">
+            <img src="{{ asset('frontend') }}/assets/images/logo/logo-dark.svg" alt="logo">
+          </a>
+        </div>
+      </div>
+      <div class="col">
+        <div class="lonyo-main-menu-item">
+          
+          <nav class="main-menu menu-style1 d-none d-lg-block menu-left">
+            <ul>
+              <li>
+                <a href="{{ url('/') }}">Home</a>
+              </li>
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Lonyo - IT Solution & Technology Temaptle</title>
+              <li class="menu-item-has-children">
+                <a href="#">About Us</a>
+                <ul class="sub-menu">
+                  <li><a href="{{ url('/about-us') }}">Company Profile</a></li>
+                  <li><a href="{{ url('/pricing') }}">Team</a></li>              
+                </ul>
+              </li>
+              <li>
+                <a href="#">Our Service</a>
+              </li> 
+              <li>
+                <a href="#">Portfolio</a>
+              </li>
+              <li>
+                <a href="#">Blog</a>
+              </li>
+              <li>
+                <a href="contact-us.html">Contact</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+      <div class="col-auto d-flex align-items-center">
+        <div class="lonyo-header-info-wraper2">
+          <div class="lonyo-header-info-content">
+            <ul>
+              <li><a href="{{ route('login') }}">Log in</a></li>
+            </ul>
+          </div>
+          <a class="lonyo-default-btn lonyo-header-btn" href="conact-us.html">Book a demo</a>
+        </div>
+        <div class="lonyo-header-menu">
+          <nav class="navbar site-navbar justify-content-between">
+            <!-- Brand Logo-->
+            <!-- mobile menu trigger -->
+            <button class="lonyo-menu-toggle d-inline-block d-lg-none">
+              <span></span>
+            </button>
+            <!--/.Mobile Menu Hamburger Ends-->
+          </nav>
+        </div>
+      </div>
+    </div>
+  </div>
 
-  <link rel="shortcut icon" href="{{ asset('frontend') }}/assets/images/favicon.ico" type="image/x-icon">
-  <link rel="icon" href="assets/images/favicon.ico" type="image/x-icon">
-  <!--- End favicon-->
-
-  <link href="https://fonts.googleapis.com/css2?family=Afacad:ital,wght@0,400..700;1,400..700&family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Instrument+Sans:ital,wght@0,400..700;1,400..700&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
-
-  <!-- End google font  -->
-  
-  <link rel="stylesheet" href="{{ asset('frontend') }}/assets/css/bootstrap.min.css">
-  <link rel="stylesheet" href="{{ asset('frontend') }}/assets/css/magnific-popup.css">
-  <link rel="stylesheet" href="{{ asset('frontend') }}/assets/css/slick.css">
-  <link rel="stylesheet" href="{{ asset('frontend') }}/assets/css/fontawesome.css">
-  <link rel="stylesheet" href="{{ asset('frontend') }}/assets/css/remixicon.css">
-  <link rel="stylesheet" href="{{ asset('frontend') }}/assets/css/aos.css">
-  <link rel="stylesheet" href="{{ asset('frontend') }}/assets/css/niceselect.css">
-  <link rel="stylesheet" href="{{ asset('frontend') }}/assets/css/animate.min.css">
-
-  <!-- Code Editor  -->
-
-  <link rel="stylesheet" href="{{ asset('frontend') }}/assets/css/main.css">
-  <link rel="stylesheet" href="{{ asset('frontend') }}/assets/css/app.min.css">
-
-</head>
+</header>
